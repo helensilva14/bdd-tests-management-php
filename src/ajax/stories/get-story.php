5d162b7f717a448da2f0d9d6d11753bd90991863
Henrique Grasso <henriquegrasso96@gmail.com>
2018-09-22 22:35:00 -0300

    Merge branch 'master' of https://github.com/helensilva14/bdd-tests-management-php

--- src/ajax/stories/get-story.php
+++ src/ajax/stories/get-story.php
@@ -1,6 +1,7 @@
 <?php
     // include database connection file
     include("../db-connection.php");
+     
     // check request
     if(isset($_GET['story']))
     {
@@ -11,25 +12,28 @@
         
         //echo $query;
 =======
+     
+        $query = "SELECT * FROM story WHERE idstory = '$id'";
+        
+        $con->query("SET CHARACTER SET utf8;");
+        $con->query("SET collation_connection = utf8_unicode_ci;");
 >>>>>>> b4d7ade8a0f952910ce4f1ace2ddc24415daa0c5
         
         if (!$result = mysqli_query($con, $query)) {
-            exit(mysqli_error($con));
+            mysqli_error($con);
         }
         
         $response = array();
         
         if(mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
-                $response = $row;
+                $response = json_encode($row);
             }
         }
         else
         {
             $response['status'] = 200;
             $response['message'] = "Nenhum dado foi encontrado";
-            
-            echo 'erro';
         }
         
         // display JSON data
